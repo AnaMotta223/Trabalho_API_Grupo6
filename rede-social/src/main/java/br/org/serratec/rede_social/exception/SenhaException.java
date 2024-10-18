7bd5881fe9c79d24d6bad1129a9e55408f861966
Vinícius Simas Vimercati <viniciussimasv@gmail.com>
2024-10-18 15:39:51 -0300

    Criado a classe SenhaException e ControllerExceptionHandler no pacote exception

---
+++ rede-social/src/main/java/br/org/serratec/rede_social/exception/SenhaException.java
@@ -0,0 +1,9 @@
+package br.org.serratec.rede_social.exception;
+
+public class SenhaException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public SenhaException(String message){
+        super(message);
+    }
+}
