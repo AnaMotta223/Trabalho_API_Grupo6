a5d2f6ec1d5bd93d5748cf354536c188c6d18a63
ChrisAvellar <2kavellargmail.com>
2024-10-18 15:59:21 -0300

    Adicionando os Repositories

---
+++ rede-social/src/main/java/br/org/serratec/rede_social/repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package br.org.serratec.rede_social.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.serratec.rede_social.domain.Postagem;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+}
